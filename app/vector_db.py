b3c9f1216f27efe2fe5ef6e719771d0a6bf79941
admin <syedmoinudeen65@gmail.com>
2026-03-30 16:09:25 +0530

    added qdrant cluster

--- app/vector_db.py
+++ app/vector_db.py
@@ -3,10 +3,17 @@ from qdrant_client.models import VectorParams , Distance
 import os
 
 
+# client = QdrantClient(
+#     host=os.getenv("QDRANT_HOST", "localhost"),
+#     port=int(os.getenv("QDRANT_PORT", 6333))
+# ) 
+
 client = QdrantClient(
-    host=os.getenv("QDRANT_HOST", "localhost"),
-    port=int(os.getenv("QDRANT_PORT", 6333))
+    url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY"),
 )
+
+
 
 def create_collection():
     if not client.collection_exists("rag_test"):
